be33256fd40964419a7f230149a9ee023424d86d
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-03-01 20:10:49 +0000

    Added stacked bar and pie charts to categorical data visualisation

--- RVisualisation/Categorical data.R
+++ RVisualisation/Categorical data.R
@@ -17,6 +17,23 @@ courses <- data.frame(areas=c('L', 'M', 'L', 'F', 'M', 'M', 'F', 'M', 'F', 'M',
                               '2015', '2016', '2014', '2016', '2014', '2016', '2016', '2014', '2016', '2015',
                               '2015', '2016', '2016', '2016', '2015', '2016', '2014', '2016', '2016', '2016'))
 
-barplot(table(courses), ylab="Count", main="Genders by Areas", col=rainbow(3), beside=TRUE, ylim=c(0,9), xlim=c(0,17))
+barplot(table(courses), ylab="Count", main="Areas By Year", col=rainbow(3), beside=TRUE, ylim=c(0,9), xlim=c(0,17))
 legend("topright", legend=levels(courses$areas), fill=rainbow(3))
 
+# Stacked bar chart
+barplot(table(courses), ylab="Count", main="Years by Areas", col=heat.colors(3), ylim=c(0,17), xlim=c(0,5))
+legend("topright", legend=levels(courses$areas), fill=heat.colors(3))
+
+# 100% stacked bar chart
+# Express Table entries as fraction of marginal table
+barplot(prop.table(bartable, 2) * 100, col=heat.colors(3), ylab="%", main="Years by Areas", xlim=c(0,5))
+legend("topright", legend=levels(courses$areas), fill=heat.colors(3))
+
+# Pie chart
+# Create the labels
+areas <- table(courses$areas)
+per_labels <- round(areas/sum(areas) * 100, 1)
+per_labels <- paste(per_labels, "%", sep="")
+
+pie(table(courses$areas), col=rainbow(3), labels=per_labels)
+legend("topright", legend=levels(courses$areas), fill=rainbow(3))
