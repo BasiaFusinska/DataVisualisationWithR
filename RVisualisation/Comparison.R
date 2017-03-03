04b5b51c0cae3e121e414445f52389608404327a
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-03-03 17:01:15 +0000

    Added graphs from lattice to comparison code

--- RVisualisation/Comparison.R
+++ RVisualisation/Comparison.R
@@ -9,8 +9,10 @@ revenue <- data.frame(total=as.numeric(c(250, 20, 35, 0, 5, 120, 550, 12, 14, 18
                               '2015', '2016', '2016', '2016', '2015', '2016', '2014', '2016', '2016', '2016'))
 
 # Aggregate data
+library(reshape)
 bardata_aggr <- aggregate(total ~ years + areas, data=revenue, sum)
-bardata <- cast(bardata_aggr, years ~ areas, value="total")
+bardata_cast <- cast(bardata_aggr, years ~ areas, value="total")
+bardata <- bardata_cast
 row.names(bardata) <- bardata$years
 bardata$years <- NULL
 
@@ -18,6 +20,13 @@ bardata$years <- NULL
 # Base
 barplot(t(as.matrix(bardata)), ylab="Count [$]", main="Areas by Year", col=rainbow(3), xlim=c(0,5))
 legend("topright", legend=names(bardata), fill=rainbow(3))
+
+# lattice
+fareas <- names(bardata_cast)
+fareas <- fareas[2:length(fareas)]
+formula <- as.formula(paste(paste(fareas, collapse= "+"), " ~ years"))
+
+barchart(formula, data=bardata_cast, stack=TRUE, horizontal=FALSE, ylab="Count [$]", main="Areas by Year")
 
 # ggplot2
 install.packages('ggplot2')
@@ -30,13 +39,28 @@ ggplot(bardata_aggr, aes(x = years, y= total, fill = areas)) +
 # base
 hist(revenue$total, main="Distribution of prices", xlab="Course price", breaks=20, col=heat.colors(20))
 
+# lattice
+histogram(~total, data=revenue, breaks = 20, type = "count", col=heat.colors(20), main="Distribution of prices", xlab="Course price")
+
 # ggplot
 ggplot(revenue, aes(x = total)) + geom_histogram(stat = "bin", binwidth=20, aes(fill=..count..))
 
 # box plot
 # base
-boxplot(total~years, data=revenue, col=1:3, main="Total by Year", 
+boxplot(total~years, data=revenue, col=2:4, main="Total by Year", 
         xlab="Year", ylab="Revenue")
+
+# lattice
+bwplot(total~years, data=revenue, main="Total by Year", 
+        xlab="Year", ylab="Revenue")
+
+# adding colours
+my.theme = list(box.dot = list(col = "black"), 
+                box.rectangle = list(fill=2:4),
+                box.umbrella = list(col = "black"))
+bwplot(total~years, data=revenue, main="Total by Year", 
+       par.settings = my.theme,
+       xlab="Year", ylab="Revenue")
 
 # ggplot
 ggplot(revenue, aes(x=years, y=total)) + 
@@ -49,6 +73,9 @@ bardata$freq <- aggregate(total~years+areas, data=revenue, length)$total
 
 # base
 plot(bardata$freq, bardata$total, xlab="Units", ylab="Revenue", col=bardata$areas)
+
+#lattice
+xyplot(total~freq, data=bardata, xlab="Units", ylab="Revenue", col=bardata$areas)
 
 # ggplot
 ggplot(bardata, aes(x=freq, y=total)) + geom_point(aes(col=areas))
