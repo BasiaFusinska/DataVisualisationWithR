c6483a17237a963ba1dc0fdf502844c7cd958d4e
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-03-04 15:28:49 +0000

    Used dataset when creating bivariate data plots

--- RVisualisation/Bivariate.R
+++ RVisualisation/Bivariate.R
@@ -1,40 +1,30 @@
-revenue <- data.frame(total=as.numeric(c(250, 20, 35, 0, 5, 120, 550, 12, 14, 18,
-                                         50, 130, 15, 44, 33, 250, 0, 99, 95, 20,
-                                         30, 200, 350, 11, 55, 0, 12, 160, 44, 180)),
-                      areas=c('L', 'M', 'L', 'F', 'M', 'M', 'F', 'M', 'F', 'M',
-                              'L', 'M', 'L', 'F', 'M', 'M', 'F', 'M', 'F', 'M',
-                              'L', 'M', 'L', 'F', 'M', 'M', 'F', 'M', 'F', 'M'),
-                      years=c('2014', '2015', '2015', '2014', '2014', '2016', '2016', '2015', '2015', '2015',
-                              '2015', '2016', '2014', '2016', '2014', '2016', '2016', '2014', '2016', '2015',
-                              '2015', '2016', '2016', '2016', '2015', '2016', '2014', '2016', '2016', '2016'))
-
-# count units and total revenue per year
-bardata <- aggregate(total~years, data=revenue, sum)
-bardata$freq <- aggregate(total~years, data=revenue, length)$total
+# count units and calculate total revenue per month in 2016
+revenue.month <- aggregate(price~month+year, data=courses.aggregate[courses.aggregate$year==2016,], sum)
+revenue.month$units <- aggregate(price~month+year, data=courses.aggregate[courses.aggregate$year==2016,], length)$price
 
 # Plot the revenue and units (* 100) in one graph
-barplot(bardata$total, names.arg = bardata$years , ylab="Count [$]", main="Revenue by year")
-lines(bardata$freq*100)
-points(bardata$freq*100)
+labels <- paste(revenue.month$month, revenue.month$year, sep = "/")
+revenue.bar <- barplot(revenue.month$price, names.arg = labels , ylab="Revenue [$]", main="2016 Revenue by month")
+lines(x=revenue.bar, y=revenue.month$units*100)
+points(x=revenue.bar, y=revenue.month$units*100)
 
-# Plot revenue per years
-x <- as.integer(as.character(bardata$years))
-plot(x, bardata$total, type='l', xaxt="n")
-axis(1, at=round(x, 0))
+# Plot revenue trend through 2016
+months <- 1:12
+plot(price ~ month, data=revenue.month, xaxt="n", type="l", ylab="Revenue [$]", xlab="", main="Revenue in 2016")
+axis(1, at=months, labels=labels)
 
 # displaying the trend
-lines(x, c(250, 1000, 1750), type='l', lty=2, col="blue")
-legend("topleft", c("revenue", "trend"), col=c("black", "blue"), lty=1:2)
+lines(c(1,12), c(25000, 12000), type='l', lty=2, col="blue")
+legend("topright", c("Revenue", "Trend"), col=c("black", "blue"), lty=1:2)
 
-# plot revenue per units number
-plot(total~freq, data=bardata, xlab="Units", ylab="Revenue")
-lines(c(7, 10, 13, 14), c(250, 1000, 1750, 2000), type='l', lty=2, col="blue")
+# Plot revenue per number of the courses send by month
+plot(price~units, data=revenue.month, xlab="Units", ylab="Revenue [$]", main="Revenue by Units in 2016")
+lines(c(30, 380), c(3000, 35000), type='l', lty=2, col="blue")
 legend("topleft", c("revenue/freq", "trend"), col=c("black", "blue"), lty=c(0,2), pch=c(21, -1))
 
 # plot revenue per units for different areas
-bardata <- aggregate(total~years + areas, data=revenue, sum)
-bardata$freq <- aggregate(total~years + areas, data=revenue, length)$total
+revenue.month.area <- aggregate(price~month+year+area, data=courses.aggregate, sum)
+revenue.month.area$units <- aggregate(price~month+year+area, data=courses.aggregate, length)$price
 
-plot(total~freq, data=bardata, xlab="Units", ylab="Revenue", col=areas)
-legend("topleft", legend=levels(bardata$areas), col=1:length(levels(bardata$areas)), pch=21)
-
+plot(price~units, data=revenue.month.area, xlab="Units", ylab="Revenue [$]", col=area, main="Revenue by Units (All years)")
+legend("topleft", legend=levels(revenue.month.area$area), col=1:length(levels(revenue.month.area$area)), pch=21, text.width = 30)
