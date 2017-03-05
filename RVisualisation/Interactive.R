79f45adba6efec82a7297db663b5e9bba5ec6d6a
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-03-05 18:31:36 +0000

    Moved interactive plots to separate file

---
+++ RVisualisation/Interactive.R
@@ -0,0 +1,27 @@
+# Scatter plot
+#not interactive graph
+library(ggplot2)
+
+ggp <- ggplot(revenue.month.vendor, aes(x=units, y=total)) + 
+  geom_point(aes(col=area, size=dltotal, text = paste("DataLearn:", dltotal)))
+ggp
+
+# Use plotly to make it interactive
+install.packages('plotly')
+library(plotly)
+
+# Make ggplot interactive
+ggplotly(ggp)
+
+ggbar <- ggplot(revenue.year.area, aes(x = year, y=price, fill = area)) + 
+  geom_bar(stat = "identity")
+ggbar
+
+ggplotly(ggbar)
+
+# Customise the graph in plotly 
+plot_ly(revenue.month.vendor, x=~units, y=~total, color = ~factor(area), 
+        size=~dltotal/1000, mode="markers", 
+        text=~paste("Units:", units,"</br>Revenue", total, "</br>DataLearn cut:", dltotal), 
+        hoverinfo="text", type='scatter') %>%
+  layout(title="Revenue per vendor", xaxis=list(title="Units"), yaxis=list(title="Revenue [$]"))
